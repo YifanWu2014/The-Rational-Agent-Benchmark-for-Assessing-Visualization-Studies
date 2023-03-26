10cbd0ad62d8893b76027801e006bd19015c6e2c
YifanWu2014 <wuyifan@dhcp-10-105-225-70.wireless.northwestern.private>
2023-03-26 18:19:40 -0500

    .

--- analysis-CI.py
+++ analysis-CI.py
@@ -15,7 +15,7 @@ percentage_heuristic_high = [1.0, -2.2298532996428855, -2.2298532996428855, -2.2
 '''
 n_data = 0
 #bin_size = 0.05
-eps = 1e-10
+eps = 1e-5
 
 rand_size = 160 * 2
 #prior_freq = 0.7518749999999998
@@ -85,10 +85,6 @@ means_data = []
 condition_data = []
 
 
-decision_ground_truth_data = []
-decision_condition_data = []
-decision_means_data = []
-decision_pred_data = []
 
 with open('prob_superiority_yifan.csv', newline='') as f:
 	data = csv.reader(f)
@@ -122,12 +118,14 @@ def binning(bin_size):
 	freq = np.zeros((4, 2, 8, 100), dtype=float)
 	for i in range(n_data):
 		pred = int (inv_log( pred_data[i]) / bin_size)
+		# counting condition, means, groundtruth, the response numbers, empirical distribution
 		freq[condition_dict[condition_data[i]], means_dict[means_data[i]], ground_truth_data[i], pred] += 1
 	return freq
 
 
-bin_size = 0.03
+bin_size = 0.02
 freq = binning(bin_size)
+# compute prior - np.sum(freq[0, 0], axis = 1) is the number of trials with each ground truth, lo_ground_truth winning prob
 prior_freq = np.inner(np.sum(freq[0, 0], axis = 1), lo_ground_truth) / np.sum(freq[0, 0])
 
 def compute_freq(bin_num, bin_size):
@@ -202,6 +200,8 @@ def calc_score(freq_tmp, bin_size):
 
 
 
+
+
 def main():
 
 	
@@ -220,44 +220,50 @@ def main():
 
 
 	
+	decision_ground_truth_dict = {}
+	decision_ground_truth_list = []
+	decision_ground_truth_count = 0
+	decision_freq = np.zeros((4, 2, 8, 2), dtype = float)
 
-
-	decision_pred = np.zeros((4, 2), dtype = float)
-	decision_pred_flip = np.zeros((4, 2), dtype = float)
-	decision_pred_cnt = np.zeros((4, 2, 8), dtype = int)
-	decision_pred_cnt_tmp = np.zeros((4, 2), dtype = int)
-	decision_pred_map = {}
-	decision_wrong_cnt = np.zeros((4, 2, 8), dtype = float)
-	bias_ind = []
 	with open('posterior_predictive_draws_decisions_kale2020.csv', newline = '') as f:
 		data = csv.reader(f)
 		headers = next(data)
 		for row in data:
-			tmp_ground_truth = inv_log(float(row[0]) + qlogis(0.5 + 1.0 / 3.17))
-			if not tmp_ground_truth in decision_pred_map:
-				decision_pred_map[tmp_ground_truth] = decision_pred_cnt_tmp[condition_dict[row[3]], means_dict[row[1]]]
-				decision_pred_cnt_tmp[condition_dict[row[3]], means_dict[row[1]]] += 1
-				bias_ind.append(tmp_ground_truth)
-			decision_pred_cnt[condition_dict[row[3]], means_dict[row[1]], decision_pred_map[tmp_ground_truth]] += 1
-			decision_wrong_cnt[condition_dict[row[3]], means_dict[row[1]], decision_pred_map[tmp_ground_truth]] += (tmp_ground_truth >= 0.81545741) ^ (int(row[10]))
-			decision_pred[condition_dict[row[3]], means_dict[row[1]]] += decision_score(int(row[10]), tmp_ground_truth)
-			decision_pred_flip[condition_dict[row[3]], means_dict[row[1]]] += decision_score(1 - int(row[10]), tmp_ground_truth)
-	#print(decision_pred_map)
-	#print(decision_pred_cnt)
+			if not row[0] in decision_ground_truth_dict:
+				tmp_ground_truth = inv_log(float(row[0]) + qlogis(0.5 + 1.0 / 3.17))
+				flag = True
+				for i in range(len(decision_ground_truth_list)):
+					t = decision_ground_truth_list[i]
+					if eq_err(t, tmp_ground_truth):
+						flag = False
+						decision_ground_truth_dict[row[0]] = i
+				if flag:
+					decision_ground_truth_list.append(tmp_ground_truth)
+					decision_ground_truth_dict[row[0]] = decision_ground_truth_count
+					decision_ground_truth_count += 1
+			decision_freq[condition_dict[row[3]], means_dict[row[1]], decision_ground_truth_dict[row[0]], int(row[10])] += 1
+
+	score_decision = []
+
 	for t in range(4):
 		for m in range(2):
-			print(decision_wrong_cnt[t, m] / decision_pred_cnt[t, m])
+			tmp_decision = []
+			for i in range(100):
+				score_tmp = 0.0
+				for j in range(0, decision_ground_truth_count):
+					sample = random.choices([0, 1], weights = decision_freq[t, m, j], k = rand_size)
+					freq_tmp = float(sum(sample)) / rand_size
+					if t == 0 and m == 0 and i == 0:
+						print(freq_tmp)
+					score_tmp += decision_score(freq_tmp, decision_ground_truth_list[j])
+				score_tmp = score_tmp / decision_ground_truth_count
+				tmp_decision.append(score_tmp)
+				if t == 0 and m == 0 and i == 0:
+					print(score_tmp)
+			tmp_decision.sort()
+			tmp_decision = tmp_decision[2:97]
+			score_decision.extend(tmp_decision)
 
-	for i in range(len(bias_ind)):
-		for j in range(i + 1, len(bias_ind)):
-			if eq_err(bias_ind[i], bias_ind[j]):
-				bias_ind.remove(bias_ind[j])
-	decision_pred = decision_pred / np.sum(decision_pred_cnt, axis = 2)
-	decision_pred_flip = decision_pred_flip / np.sum(decision_pred_cnt, axis = 2)
-	decision_score_ind = []
-	decision_score_final = []
-	decision_score_final_flip = []
-	
 
 	score_bounds = [prior_score, posterior_score]
 	score_bounds_ind = [0, 0]
@@ -283,26 +289,21 @@ def main():
 			score_behav.extend(tmp_behav)
 			score_calib.extend(tmp_calib)
 
-			#decision_score_final.append(decision_score(decision_pred[t, m], list(range(0, n_ground_truth))))
-			decision_score_ind.append(t * 4 + m + 1)
-			decision_score_final.append(decision_pred[t, m])
-			decision_score_final_flip.append(decision_pred_flip[t, m])
-			#decision_score_final.append(decision_score_tmp[(means[m], condition[t])] / decision_count_tmp[(means[m], condition[t])])
-			#decision_score_ind.append(t * 4 + m + 1)
-	print(decision_score_final)
-	#area.extend([1] * (95 * 8))
 	plt.scatter(score_bounds_ind, score_bounds, s = 100)
 	plt.scatter(score_ind, score_calib, s = 1)
 	plt.scatter(score_ind, score_behav, s = 1)
-	plt.scatter(decision_score_ind, decision_score_final, s = 50)
+	plt.scatter(score_ind, score_decision, s = 2)
+	#plt.scatter(decision_score_ind, decision_score_final, s = 50)
 	#plt.scatter(decision_score_ind, decision_score_final_flip, s=30)
 	plt.show()
 
 
+'''
 	for t in range(4):
 		for m in range(2):
 			plt.subplot(8, 1, t * 2 + m  + 1)
 			plt.plot(bias_ind, decision_wrong_cnt[t, m] / decision_pred_cnt[t, m])
 	plt.show()
+'''
 
 main()
